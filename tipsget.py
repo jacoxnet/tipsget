c1cde4c4038d719e3ce7b5b2faad99fdc5c66c78
James A. Cox <jacox@jacox.net>
2026-03-01 18:13:22 -0600

    adding st louis fed api

--- tipsget.py
+++ tipsget.py
@@ -1,13 +1,17 @@
 import requests
 import csv
 import datetime
+import os
 
 filename = "tips.csv"
 default = {}
-baseUrl = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service'
+baseUrlt = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service'
 summary_endpoint = '/v1/accounting/od/tips_cpi_data_summary'
 details_endpoint = '/v1/accounting/od/tips_cpi_data_detail'
-sfields = ["cusip", "interest_rate", "security_term", "series", "original_issue_date", "maturity_date"]
+baseUrlfed = 'https://api.stlouisfed.org/fred/series/observations'
+fedAPIkey = os.getenv("SFEDKEY")
+sfields = ["cusip", "interest_rate", "security_term", "series", "original_issue_date", "maturity_date"
+           "dated_date", "ref_cpi_on_dated_date"]
 ifields = ["index_date", "index_ratio"]
 mikefields = {"cusip": "Cusip", "interest_rate": "Coupon", "maturity_date": "Maturity Date", 
               "security_term": "Term", "series": "Series", "original_issue_date": "Issue Date",
@@ -16,7 +20,7 @@ my_tips = []
 
 def get_all_tips():
     print("Getting summary TIPS data...")
-    API = baseUrl + summary_endpoint
+    API = baseUrlt + summary_endpoint
     parstring = ""
     response = requests.get(API + parstring)
     tips_list = response.json()["data"]
@@ -29,6 +33,17 @@ def get_indexes(the_date):
     response = requests.get(API + parstring)
     index_list = response.json()["data"]
     return index_list
+
+def get_cpiu(the_date):
+    print("Getting CPI-U data ...")
+    API = baseUrlfed
+    parstring = f"?series_id=CPIAUCSL&observation_start={the_date}&observation_end={the_date}&api_key=YOUR_API_KEY&file_type=json"
+    response = requests.get(API + parstring)
+    cpiu_data = response.json()
+    if cpiu_data["observations"]:
+        return cpiu_data["observations"][0]["value"]
+    else:
+        return None
 
 def find_index(cusip, index_list):
     for index_item in index_list:
